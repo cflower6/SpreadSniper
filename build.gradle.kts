3a4cd2727f2a18bfe5d757f46b24146ea9309fb8
chris <christopher.j.flowers6@gmail.com>
2025-07-04 16:03:28 -0400

    Updated grade and removed unused Main.kt

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
 }
 
 application {
-    mainClass.set("SpreadSniperKt") // Match your entrypoint filename
+    mainClass.set("SpreadSniperkt") // Match your entrypoint filename
 }
 
 tasks.test {
