94896a2197c4a1815fefef6dbdc658b555ed24ed
chris <christopher.j.flowers6@gmail.com>
2026-08-29 22:26:32 -0400

    Needed to added the emits for all 3 events

--- src/main/kotlin/orchestrator/OpportunityOrchestrator.kt
+++ src/main/kotlin/orchestrator/OpportunityOrchestrator.kt
@@ -48,8 +48,10 @@ class OpportunityOrchestrator(private val eventBus: EventBus) {
                 "%.6f".format(opp.sellQuote))
 
             val now = System.currentTimeMillis()
-            if (now - lastEmailMs > AppConfig.emailCooldownMs) eventBus.emit(OpportunityEvent.OpportunityFound(opp))
+            if (now - lastEmailMs > AppConfig.emailCooldownMs) eventBus.emit(OpportunityEvent.Notification(opp))
             else logger.debug("Notification event was skipped")
+            eventBus.emit(OpportunityEvent.OpportunityFound(opp))
+            eventBus.emit(OpportunityEvent.ExecuteOpportunity(opp))
         } ?: run {
             logNoOpportunities(found)
         }
